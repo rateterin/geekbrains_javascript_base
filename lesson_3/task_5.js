42b45c8b0b5d81e686b4d769889195d34ec2ef08
Роман Тетерин <rateterin@yandex.ru>
2021-04-13 21:05:00 +0300

    added tasks 1 - 5 for lesson 3

---
+++ lesson_3/task_5.js
@@ -0,0 +1,13 @@
+/* 5.
+* Нарисовать пирамиду с 20 рядами с помощью console.log, как показано на рисунке:
+x
+xx
+xxx
+xxxx
+xxxxx
+*/
+
+
+for (var i = 1; i <= 20; i++) {
+    console.log('x'.repeat(i))
+}
